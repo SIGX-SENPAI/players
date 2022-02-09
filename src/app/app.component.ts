df98063d41a887e946e4a118d33c71c8aa220dc3
SIGX-SENPAI <christian2011b@hotmail.com>
2022-02-08 19:15:41 -0500

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import Player from '@vimeo/player'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'proyectoTest';
+
+  ngOnInit() {
+    const options = {
+      id: 657768349,
+      width: 640,
+      loop: true
+    };
+
+    const player = new Player('made-in-ny', options);
+    player.ready().then(function () {
+      console.log('ready');
+    });
+
+    //   player.loadVideo(options).then(function(id) {
+    //     console.log('id', id);
+    //   }).catch(function(error) {
+    //     switch (error.name) {
+    //         case 'TypeError':
+    //             console.log('Error de id');
+    //             break;
+
+    //         case 'PasswordError':
+    //             console.log('Error de password');
+    //             break;
+
+    //         case 'PrivacyError':
+    //             console.log('Error de privacidad');
+    //             break;
+
+    //         default:
+    //             console.log('Otro error', error);
+    //           break;
+    //     }
+    // });
+    const onPlay = function (data) {
+      console.info('data', data);
+    };
+    player.on('play', onPlay);
+
+    // player.setVolume(0);
+
+    player.pause().then(function () {
+      console.log('pausa');
+    })
+
+    player.play().then(function() {
+      console.log('played video');
+  })
+  }
+
+  unload() {
+    const player = new Player('made-in-ny');
+    player.unload().then(function () {
+      console.log('unload');
+    }).catch(function (error) {
+    });
+  }
+
+  ngOnDestroy() {
+    const player = new Player('made-in-ny');
+    player.destroy().then(function () {
+      console.log('destruido');
+    }).catch(function (error) {
+      // an error occurred
+    });
+  }
 }
--- src/app/vjs-player/vjs-player.component.ts
+++ src/app/vjs-player/vjs-player.component.ts
@@ -36,53 +36,17 @@ export class VjsPlayerComponent implements OnInit, OnDestroy {
   ngOnInit() {
     // instantiate Video.js
     this.player = videojs(this.target.nativeElement, this.options, function onPlayerReady() {
-      console.log('onPlayerReady', this);
+      // console.log('onPlayerReady', this);
     });
     this.player.seekButtons({
       forward: 30,
       back: 10
     });
-    videojs('video', {
-      controls: true,
-      plugins: {
-          videoJsResolutionSwitcher: {
-            default: 'high',
-            dynamicLabel: true
-          }
-        }
-    }, function(){
-
-      // Add dynamically sources via updateSrc method
-      this.player.updateSrc([
-          {
-            src: 'http://media.xiph.org/mango/tears_of_steel_1080p.webm',
-            type: 'video/webm',
-            label: '360'
-          },
-          {
-            src: 'http://mirrorblender.top-ix.org/movies/sintel-1024-surround.mp4',
-            type: 'video/mp4',
-            label: '720'
-          }
-        ])
-
-        this.player.on('resolutionchange', function(){
-          console.info('Source changed to %s', this.player.src())
-        })
-
-    });
     // this.player.currentTime (120)
   }
   ngAfterViewChecked() {
-    console.log('duracion', this.player.currentTime ());
     this.player.on('ended', function() {
-      console.log('video muerto')
     });
-    // for (let i = 0; i < this.player.length; i++) {
-    //   this.player[i].addEventListener("ended", () =>{
-    //     console.log('deded<sa');
-    //   });
-    // }
   }
   ngOnDestroy() {
     // destroy player
